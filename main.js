64aae92fe85f4d2103d7d8e3dd4ba503c6b0253b
Avus <avuwake@gmail.com>
2022-06-21 17:05:32 +0100

    catch update

--- main.js
+++ main.js
@@ -9,26 +9,27 @@ operator = prompt('Enter one operator i.e  +  or  -  or  *  or  /')
 secondNumber = parseInt(prompt(`Enter second number`)) 
 
 //checking for the operation choosen by the user
-if(operator == '+'){
-    result = firstNumber + secondNumber
-    alert(`${firstNumber} ${operator} ${secondNumber} = ${result}`)
-}
-else if(operator == '-'){
-    result = firstNumber - secondNumber
-    alert(`${firstNumber} ${operator} ${secondNumber} = ${result}`)
-}
-else if(operator == '*'){
-    result = firstNumber * secondNumber
-    alert(`${firstNumber} ${operator} ${secondNumber} = ${result}`)
-}
-else if (operator == '/'){
-    result = firstNumber / secondNumber
-    alert(`${firstNumber} ${operator} ${secondNumber} = ${result}`)
+if(isNaN(firstNumber) || isNaN(secondNumber)){
+    alert(`Invalid Operation: ${firstNumber} ${operator} ${secondNumber}. Kindly restart`)
 }
 else{
-    alert(`Invalid Operation: ${firstNumber} ${operator} ${secondNumber}`)
+    if(!(operator == '+') && !(operator == '-') && !(operator == '*') && !(operator == '/')){
+        alert(`Invalid Operation: ${firstNumber} ${operator} ${secondNumber}. Kindly restart`)
+    }
+    else{
+        if(operator == '+'){
+            result = firstNumber + secondNumber
+        }
+        else if(operator == '-'){
+            result = firstNumber - secondNumber
+        }
+        else if(operator == '*'){
+            result = firstNumber * secondNumber
+        }
+        else if(operator == '/'){
+            result = firstNumber / secondNumber
+        }
+        alert(`${firstNumber} ${operator} ${secondNumber} = ${result}`)
+    }
 }
-//Log full result of calcution
-console.log(`${firstNumber} ${operator} ${secondNumber} = ${result}`)
-
 
